ee036aeb7a3cfb003e27786230dd0989cab6dfba
cpress <cpress@it4ipm.de>
2020-06-01 14:26:23 +0200

    Adding an Auth Guard - simple with redirect - beware mem leaks

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,7 +2,7 @@ import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTre
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {AuthService} from './auth.service';
-import {map, tap} from 'rxjs/operators';
+import {map, take, tap} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
@@ -10,7 +10,7 @@ export class AuthGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
     : Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.user.pipe(map(user => {
+    return this.authService.user.pipe(take(1), map(user => {
       const isAuth = !!user;
       if (isAuth) {
         return true;
